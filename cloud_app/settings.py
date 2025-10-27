a9f87e99fe6905a9d48000e3aa45f1847cf1bc8a
iara.vsp <iaravsp@al.insper.edu.br>
2025-10-27 17:58:31 -0300

    mudando banco

--- cloud_app/settings.py
+++ cloud_app/settings.py
@@ -79,11 +79,14 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'banco1',
+        'USER': 'cloud_new',
+        'PASSWORD': 'rafa',
+        'HOST': '192.169.0.72', 
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
