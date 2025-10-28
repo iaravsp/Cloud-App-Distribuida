4f5fc4d8383ea4ea6aec33e26e7f271129d7a365
iara.vsp <iaravsp@al.insper.edu.br>
2025-10-28 15:30:43 -0300

    teste

--- frontend-react-cloudapp/src/services/api.js
+++ frontend-react-cloudapp/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Use VITE_API_BASE_URL from your .env file (e.g., http://LOAD_BALANCER_PUBLIC_IP/api)
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api'
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL,
